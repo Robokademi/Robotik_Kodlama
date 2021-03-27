e85f23d39ffc47033e7f533bd2ea2eee5db199bc
Mustafa Şamil İleri <44174659+ileri95@users.noreply.github.com>
2021-03-27 18:16:35 +0300

    Add files via upload

---
+++ Week-1/test.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+
+
+void sum(int a,int b,int c){
+	int toplam=a+b+c;
+	printf("%d",toplam);
+	
+}
+
+int toplam(int a,int b,int c){
+	int toplam=a+b+c;
+	return topla;
+	 
+}
+
+
+
+
+int main(){
+
+
+//sum(150,250,600);
+
+/*
+int mass;
+scanf("%d",&mass);
+printf("mass =%d\n",mass);
+if(mass<2600){
+	mass=(mass*0.4)+mass;
+	printf("yeni maas =%d",mass);	
+}else if(mass>=2600 && mass<4500){
+	mass=(mass*1.3);
+	printf("yeni maas =%d",mass);
+}else{
+	mass=(mass*1.2);
+	printf("yeni maas =%d",mass);
+}
+		char deger;
+		scanf("%c",&deger);	 
+	switch(deger){
+		
+		case 'a':
+			printf("FSMVU\n");
+			break;
+			case 'b':
+				printf("Robokademi\n");
+				break;
+				case 'c' :
+					printf("mert \n");
+					break;
+	default :
+		printf("gecersiz islem");
+		break;
+	
+	
+	printf("RBM BANKA hosgeldiniz !!!\n");
+	printf("islem seciniz \n");
+	printf("para yatirma icin (1)\n");
+	printf("pata cekmek icin (2)\n");
+	printf("bakiye ogernmek icin(3)\n");
+int miktar;	
+int islem;
+int bakiye=10000;
+scanf("%d",&islem);
+switch(islem){
+	case 1:
+			printf("Bakiyeniz : %d\n ",bakiye);
+			printf("miktar giriniz = ");
+			scanf("%d",&miktar);
+			bakiye+=miktar;
+			printf(" Yeni Bakiyeniz : %d",bakiye);
+			break;
+	case 2:
+			printf("Bakiyeniz : %d\n ",bakiye);
+			printf("miktar giriniz = ");
+			scanf("%d",&miktar);
+			if(bakiye-miktar<0){
+				printf("yeterli bakiyen tekrar dene");
+			}else{
+					bakiye-=miktar;
+			}
+				printf(" Yeni Bakiyeniz : %d",bakiye);
+				break;
+				case 3 :
+					printf("Bakiyeniz : %d\n ",bakiye);
+					break;
+	default :
+		printf("gecersiz islem");
+		break;
+		*/
+		
+		
+	/*	
+		int i=0;
+		while(i<=10){
+			printf("%d ",i);
+			i=i+1;
+			i+=1;
+		}
+			printf("\n");*/
+			
+		/*				
+		int j;
+		for(j=21;j<=50;j+=2){
+			printf("%d ",j);
+		}
+		
+		printf("\n");
+		int m;
+		for(m=24;m<=50;m++){
+			if(m%2==0){
+				printf("%d ",m);
+			}
+		}
+		
+		
+		
+		int notlar[3];
+			notlar[0]=80;
+			notlar[1]=70;
+			notlar[2]=100;
+			
+			printf("%d\n",notlar[0]);
+			printf("%d\n",notlar[1]);
+			printf("%d\n",notlar[2]);
+			printf("\n");
+	int m;
+	for(m=0;m<3;m++){
+		printf("%d\n",notlar[m]);
+	}
+		
+	*/
+
+
+	
+}
